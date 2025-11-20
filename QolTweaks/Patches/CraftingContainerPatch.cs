1a357291b6e71dd1a60d19ba5a306e1dbc8c5872
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-19 21:52:33 -0800

    figured out how to access private variables

--- QolTweaks/Patches/CraftingContainerPatch.cs
+++ QolTweaks/Patches/CraftingContainerPatch.cs
@@ -14,12 +14,13 @@ internal static class CraftingContainerPatches
     private static class AttemptCraftPatch
     {
         [HarmonyPrefix]
-        private static void Prefix(CraftingContainer __instance, RecipeSlot recipeSlot, UISlot slot)
+        private static void Prefix(CraftingContainer __instance, ref bool ___refreshQueued, RecipeSlot recipeSlot, UISlot slot)
         {
             if (!slot.IsEmpty() && InputManager.placeModifier.IsDown() && !InputManager.ui_secondary.IsDown()) {
                 int successes = 0;
                 while (recipeSlot.recipe.TryCraft(__instance.inventoryAbstraction, out ItemStack resultingItem)) {
                     __instance.playerInventory.TryAddItem(resultingItem, out ItemStack _);
+                    ___refreshQueued = true;
                     __instance.Refresh();
                     successes++;
                 }
