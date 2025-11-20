6989ad3152b8897f69ea159ecccf8c2387d3a558
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-19 21:49:33 -0800

    implement craft all

---
+++ QolTweaks/Patches/CraftingContainerPatch.cs
@@ -0,0 +1,32 @@
+using Allumeria;
+using Allumeria.UI.Containers;
+using Allumeria.Items;
+using Allumeria.Input;
+using HarmonyLib;
+
+namespace QolTweaks.Patches;
+
+// https://harmony.pardeike.net/articles/intro.html
+[HarmonyPatch]
+internal static class CraftingContainerPatches
+{
+    [HarmonyPatch(typeof(CraftingContainer), nameof(CraftingContainer.AttemptCraft))]
+    private static class AttemptCraftPatch
+    {
+        [HarmonyPrefix]
+        private static void Prefix(CraftingContainer __instance, RecipeSlot recipeSlot, UISlot slot)
+        {
+            if (!slot.IsEmpty() && InputManager.placeModifier.IsDown() && !InputManager.ui_secondary.IsDown()) {
+                int successes = 0;
+                while (recipeSlot.recipe.TryCraft(__instance.inventoryAbstraction, out ItemStack resultingItem)) {
+                    __instance.playerInventory.TryAddItem(resultingItem, out ItemStack _);
+                    __instance.Refresh();
+                    successes++;
+                }
+                if (successes > 0) {
+                    __instance.PlayShuffleSound();
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Mod for [Allumeria](https://store.steampowered.com/app/3516590/Allumeria/), usin
 ## Installation
 Download and unzip the zip file from the mod's [Releases](https://github.com/AlurienFlame/qol-tweaks/releases), and place the resulting folder in the `mods` folder of your Allumeria install. You should have created a `mods` folder when installing Ignitron.
 ## Features
+- **Craft All**: Press `Shift+LMB` to craft as many as possible of a recipe.
+### Hotkeys
 - **Sort hotkey**: Press `Mouse3` while in your inventory to sort it.
 - **Quick stack to nearby chests hotkey**: Press `G` while not in any menus to quick stack to nearby chests.
 - **Quick torch hotkey**: Press `F` to place or throw a torch directly from your hotbar. Favors items further to the left.
