41a1dc3f97e63ba438180fc1fd411dbb0e16eef8
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-20 16:12:49 -0800

    implement WAILA feature

---
+++ QolTweaks/Patches/InGameHUDPatch.cs
@@ -0,0 +1,30 @@
+using Allumeria;
+using Allumeria.ChunkManagement;
+using Allumeria.UI;
+using Allumeria.UI.Menus;
+using Allumeria.UI.Text;
+using HarmonyLib;
+
+[HarmonyPatch]
+internal static class InGameHUDPatch
+{
+    [HarmonyPatch(typeof(InGameHUD), nameof(InGameHUD.Render))]
+    public class InGameHUD_Render_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (World.player != null && !World.player.inMenu && World.player.isLookingAtBlock && World.lookingAtBlock != null && Game.worldManager.world.bossEntity == null)
+            {
+                string text = World.lookingAtBlock.item.translatedName;
+                TextRenderer.DrawTextShadow(
+                    text,
+                    UIManager.scaledWidth / 2 - TextRenderer.GetTextWidth(text) / 2,
+                    10,
+                    UIManager.scale,
+                    applyScaleToPos: true
+                );
+            }
+        }
+    }
+}
