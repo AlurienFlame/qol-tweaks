09d734b98345a19ed54ea6d59ca8f6422a8cc59d
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2025-11-20 17:22:19 -0800

    detect entities for WAILA

--- QolTweaks/Patches/InGameHUDPatch.cs
+++ QolTweaks/Patches/InGameHUDPatch.cs
@@ -1,9 +1,12 @@
 using Allumeria;
 using Allumeria.ChunkManagement;
+using Allumeria.EntitySystem;
+using Allumeria.Rendering;
 using Allumeria.UI;
 using Allumeria.UI.Menus;
 using Allumeria.UI.Text;
 using HarmonyLib;
+using OpenTK.Mathematics;
 using QolTweaks.Patches;
 
 [HarmonyPatch]
@@ -15,16 +18,57 @@ internal static class InGameHUDPatch
         [HarmonyPostfix]
         public static void Postfix()
         {
-            if (GamePatches.enable_waila.value && World.player != null && !World.player.inMenu && World.player.isLookingAtBlock && World.lookingAtBlock != null && Game.worldManager.world.bossEntity == null)
+            if (GamePatches.enable_waila.value && World.player != null && !World.player.inMenu && Game.worldManager.world.bossEntity == null)
             {
-                string text = World.lookingAtBlock.item.translatedName;
-                TextRenderer.DrawTextShadow(
-                    text,
-                    UIManager.scaledWidth / 2 - TextRenderer.GetTextWidth(text) / 2,
-                    10,
-                    UIManager.scale,
-                    applyScaleToPos: true
+                string text = "";
+                bool didFindEntity = Game.worldManager.world.chunkManager.EntityRaycast(
+                    World.player,
+                    5.5f, // player reach is a magic number :(
+                    Game.camera.position,
+                    Game.camera.front,
+                    out Entity outEntity,
+                    10
                 );
+                bool didFindBlock = World.player.isLookingAtBlock;
+                // Get block or entity name
+                if (didFindEntity && didFindBlock)
+                {
+                    // find closest
+                    float entityDist = (outEntity.position - Game.camera.position).Length;
+                    float blockDist = (World.raycastHitPositionPrecise - Game.camera.position).Length;
+
+                    // Matrix4 rotationMatrix = Matrix4.CreateScale(0.25f + World.player.selectScale);
+                    // Drawing.debugCube.Draw(WorldRenderer.blockEntityShader, Drawing.defaultTextureNoMipMap, outEntity.position, rotationMatrix);
+                    // Drawing.debugCube.Draw(WorldRenderer.blockEntityShader, Drawing.defaultTextureNoMipMap, World.raycastHitPositionPrecise, rotationMatrix);
+
+                    if (entityDist <= blockDist)
+                    {
+                        text = outEntity.GetType().Name;
+                    }
+                    else
+                    {
+                        text = World.lookingAtBlock.item.translatedName;
+                    }
+                } else if ( didFindEntity )
+                {
+                    text = outEntity.GetType().Name;
+                } else if ( World.player.isLookingAtBlock && World.lookingAtBlock != null )
+                {
+                    text = World.lookingAtBlock.item.translatedName;
+                }
+                
+                
+                // Render text
+                if (text != "")
+                {
+                    TextRenderer.DrawTextShadow(
+                        text,
+                        UIManager.scaledWidth / 2 - TextRenderer.GetTextWidth(text) / 2,
+                        10,
+                        UIManager.scale,
+                        applyScaleToPos: true
+                    );
+                }
             }
         }
     }
